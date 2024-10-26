98cd7816e26f630f308dc981d289514724ed604e
topsykrypts <142960718+gitmeED331@users.noreply.github.com>
2024-10-26 09:38:56 -0500

    improved css hot reload

--- app.ts
+++ app.ts
@@ -1,23 +1,54 @@
 #!/usr/bin/gjs -m
 import { App } from "astal/gtk3";
 import { execAsync, monitorFile } from "astal";
-// import style from "./style/main.scss";
 
 /* CSS Hot Reload */
-async function monitorStyle() {
-	const Globals = `${SRC}/style/globals`; // monitored/read for changes
-	const Components = `${SRC}/style/components`; // monitored/read for changes
-	const variables = `${SRC}/style/variables.scss`; // monitored/read for changes
-	const scss = `${SRC}/style/main.scss`; // monitored/read for changes
-	const css = `/tmp/style.css`; // output file
+// async function monitorStyle() {
+// 	const Globals = `${SRC}/style/globals`; // monitored/read for changes
+// 	const Components = `${SRC}/style/components`; // monitored/read for changes
+// 	const Variables = `${SRC}/style/variables.scss`; // monitored/read for changes
+// 	const Main = `${SRC}/style/main.scss`;
+// 	const scss = `${SRC}/style/main.scss`; // monitored/read for changes
+// 	const css = `/tmp/style.css`; // output file
 
-	monitorFile(Globals, () => transpileAndApply());
-	monitorFile(Components, () => transpileAndApply());
-	monitorFile(variables, () => transpileAndApply());
+// 	monitorFile(Globals, () => transpileAndApply());
+// 	monitorFile(Components, () => transpileAndApply());
+// 	monitorFile(Variables, () => transpileAndApply());
+// 	monitorFile(Main, () => transpileAndApply());
+
+// 	async function transpileAndApply() {
+// 		try {
+// 			await execAsync(`sass ${scss} ${css}`);
+// 			App.apply_css(css, true);
+// 			print("CSS applied successfully!");
+// 		} catch (error) {
+// 			print("Error transpiling SCSS:", error);
+// 			execAsync(`notify-send -u critical "Error transpiling SCSS" "${error}"`);
+// 		}
+// 	}
+// 	return transpileAndApply();
+// }
+
+async function monitorStyle() {
+	const pathsToMonitor = [
+		`${SRC}/style/globals`,
+		`${SRC}/style/components`,
+		`${SRC}/style/variables.scss`,
+	]; // monitored/read for changes
+	const mainScss = `${SRC}/style/main.scss`; // SCSS input file to compile
+	const css = `/tmp/style.css`; // CSS output file
+
+	let debounceTimeout;
+	function debouncedTranspileAndApply() {
+		clearTimeout(debounceTimeout);
+		debounceTimeout = setTimeout(transpileAndApply, 100);
+	}
+
+	pathsToMonitor.forEach(path => monitorFile(path, debouncedTranspileAndApply));
 
 	async function transpileAndApply() {
 		try {
-			await execAsync(`sass ${scss} ${css}`);
+			await execAsync(`sass ${mainScss} ${css}`);
 			App.apply_css(css, true);
 			print("CSS applied successfully!");
 		} catch (error) {
@@ -43,7 +74,6 @@ import {
 } from "./modules/Windows/index";
 
 App.start({
-	// css: style,
 	main() {
 		Bar({ monitor: 0 });
 		cliphist({ monitor: 0 });
